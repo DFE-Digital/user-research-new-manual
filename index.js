205587e544ac52d3c7f745223fd5267376323f82
Andy Jones <andy.jones@digital.education.gov.uk>
2024-04-29 12:16:01 +0100

    remove puppeteer

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const fs = require('fs')
 const path = require('path')
 const cheerio = require('cheerio')
 const config = require('./app/config')
-const puppeteer = require('puppeteer');
 const glob = require('glob');
 const forceHttps = require('express-force-https');
 const compression = require('compression');
